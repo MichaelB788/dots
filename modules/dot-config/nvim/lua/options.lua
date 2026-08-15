6346cd84b2ee871f07bbf93549a3a2596e298f1f
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-08-15 00:30:23 -0400

    Using 4 spaces

--- modules/dot-config/nvim/lua/options.lua
+++ modules/dot-config/nvim/lua/options.lua
@@ -94,8 +94,7 @@ vim.o.wrap = false
 vim.o.linebreak = true
 vim.o.backspace = "indent,eol,start"
 
--- Tab settings
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
 vim.o.expandtab = true
