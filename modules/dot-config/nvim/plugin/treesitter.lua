75816302417ff9647ee04b73a6a4d11a11e7498f
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-08-15 17:06:29 -0400

    Fixed pattern matching in treesitter

--- modules/dot-config/nvim/plugin/treesitter.lua
+++ modules/dot-config/nvim/plugin/treesitter.lua
@@ -3,6 +3,16 @@ vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" }, { confirm
 require('nvim-treesitter').install { 'rust', 'cpp', 'c', 'python' }
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'h*', 'c*', 'rs', 'py' },
+  pattern = {
+      'h',
+      'c',
+      'hpp',
+      'cpp',
+      'cxx',
+      'cc',
+      'c++',
+      'rs',
+      'py'
+  },
   callback = function() vim.treesitter.start() end,
 })
