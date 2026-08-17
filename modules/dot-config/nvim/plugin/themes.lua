ed773ddbabbde5ad361dd5301e110a9334628042
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-08-17 19:19:41 -0400

    Removed the aliases in favour of throwing all the themes inside themes.lua

--- modules/dot-config/nvim/plugin/themes.lua
+++ modules/dot-config/nvim/plugin/themes.lua
@@ -1,6 +1,24 @@
 vim.pack.add({
-  "https://github.com/rebelot/kanagawa.nvim",
-  "https://github.com/catppuccin/nvim",
-  "https://github.com/folke/tokyonight.nvim",
-  "https://github.com/sainnhe/everforest"
+    "https://github.com/rebelot/kanagawa.nvim",
+    "https://github.com/folke/tokyonight.nvim",
+    "https://github.com/olivercederborg/poimandres.nvim",
+    "https://github.com/ribru17/bamboo.nvim"
 }, { confirm = false })
+
+local theme=vim.cmd.colorscheme
+
+-- Uncomment one of the lines below to select a colorscheme
+
+--theme "kanagawa-wave"
+--theme "kanagawa-lotus"
+--theme "kanagawa-dragon"
+
+--theme "tokyonight-day"
+--theme "tokyonight-moon"
+--theme "tokyonight-night"
+--theme "tokyonight-storm"
+
+--theme "poimandres"
+
+--theme "bamboo"
+
