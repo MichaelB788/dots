c29ab4c184456bf9771a5c8ff8047aca15bae83e
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-08-25 18:24:46 -0400

    Changed tab length

--- modules/dot-config/nvim/lua/options.lua
+++ modules/dot-config/nvim/lua/options.lua
@@ -94,9 +94,9 @@ vim.o.wrap = false
 vim.o.linebreak = true
 vim.o.backspace = "indent,eol,start"
 
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.softtabstop = 4
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
 vim.o.expandtab = true
 
 -- Diagnostic Config
